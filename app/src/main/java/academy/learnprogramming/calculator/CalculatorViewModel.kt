7e59dc8d77e6b2de6be0d8d7e4938405eb084e56
Thananon Kumkrong <thananon.oat@gmail.com>
2019-11-13 17:21:47 +0700

    Use LiveData.

--- app/src/main/java/academy/learnprogramming/calculator/CalculatorViewModel.kt
+++ app/src/main/java/academy/learnprogramming/calculator/CalculatorViewModel.kt
@@ -1,6 +1,8 @@
 package academy.learnprogramming.calculator
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class CalculatorViewModel : ViewModel() {
@@ -9,9 +11,18 @@ class CalculatorViewModel : ViewModel() {
     private var operand1: Double? = null
     private var pendingOperation = "="
 
-    val result = MutableLiveData<String>()
-    val newNumber = MutableLiveData<String>()
-    val operation = MutableLiveData<String>()
+    private val result = MutableLiveData<Double>()
+    val stringResult: LiveData<String>
+        get() = Transformations.map(result) { it.toString()}
+
+    private val newNumber = MutableLiveData<String>()
+    val stringNewNumber: LiveData<String>
+        get() = newNumber
+
+    private val operation = MutableLiveData<String>()
+    val stringOperation: LiveData<String>
+        get() = operation
+
 
     fun digitPressed(caption: String) {
         if(newNumber.value != null) {
@@ -79,7 +90,7 @@ class CalculatorViewModel : ViewModel() {
                 "+" -> operand1 = operand1!! + value
             }
         }
-        result.value = operand1.toString()
+        result.value = operand1
         newNumber.value = ""
     }
 }
